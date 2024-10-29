9f7db209ef2b18f74d538daf882194b3d11a77e3
OneleTshukula <OneleTshukula@DESKTOP-9DU6LHU>
2024-10-29 13:57:31 +0200

    Add project files.

---
+++ ci-cd pipelines/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ci_cd_pipelines.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
